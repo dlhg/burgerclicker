2534b13554b3cd1910ae32406e717bbc44806b51
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2023-10-09 21:19:52 -0400

    first??

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+/*
+
+
+*/
+
+import { useState, useEffect } from 'react'
+import './App.css'
+
+//image imports
+import burgerpic from "./assets/images/burger.jpeg"
+import workerpic from "./assets/images/worker.jpeg"
+
+//component imports
+import Burger from './components/Burger'
+
+
+function App() {
+  const [burgerCount, setBurgerCount] = useState(0)
+  const [workers, setWorkers] = useState(0)
+
+  return (
+    <>
+      <div className="parent">
+        <div className="div1" id="burger">
+          <Burger
+            burgerCount={burgerCount}
+
+          />
+        </div>
+        <div className="div2">
+          <p>div2</p>
+        </div>
+        <div className="div3">
+          <p>div3</p>
+        </div>
+        <div className="div4">
+          <p>div4</p>
+        </div>
+        <div className="div5">
+          <p>div5</p>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Burger.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function burger(props) {
+    return (
+        <div>
+            <section>Burgers: {props.burgerCount}</section>
+
+        </div>
+    )
+}
