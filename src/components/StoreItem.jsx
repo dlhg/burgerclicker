20654b4246305597d51af0ae4db84e75abfbe559
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2023-10-10 23:04:01 -0400

    <></>

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function StoreItem(props) {
     return (
-        <div>img</div>
-        <div>item name</div>
-        <div>item price</div>
-        <div>quantity owned</div>
+        <>
+            <div>img</div>
+            <div>item name</div>
+            <div>item price</div>
+            <div>quantity owned</div>
+        </>
     )
 }
