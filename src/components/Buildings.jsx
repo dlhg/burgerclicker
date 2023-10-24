d6bea72e10aa1e4d41ca303b473edbe37ccfffad
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2023-10-24 00:44:39 -0400

    buildings section now works for all current building types

--- src/components/Buildings.jsx
+++ src/components/Buildings.jsx
@@ -6,14 +6,32 @@ export default function Buildings(props) {
             <div id="Buildings--container">
                 <div id="Building1">
                     {Array.from({ length: props.pointerCount }, (_, index) => (
-                        <div key={index} className="pointer-image">
+                        <div key={index} className="pointer--image">
                             <img src={props.pointerpic} alt={`Pointer ${index + 1}`} />
                         </div>
                     ))}
                 </div>
-                <div id="Building2">Building 2</div>
-                <div id="Building3">Building 3</div>
-                <div id="Building4">Building 4</div>
+                <div id="Building2">
+                    {Array.from({ length: props.workerCount }, (_, index) => (
+                        <div key={index} className="worker--image">
+                            <img src={props.workerpic} alt={`Worker ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
+                <div id="Building3">
+                    {Array.from({ length: props.grillCount }, (_, index) => (
+                        <div key={index} className="grill--image">
+                            <img src={props.grillpic} alt={`Grill ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
+                <div id="Building4">
+                    {Array.from({ length: props.bankCount }, (_, index) => (
+                        <div key={index} className="bank--image">
+                            <img src={props.bankpic} alt={`Bank ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
                 <div id="Building5">Building 5</div>
                 <div id="Building6">Building 6</div>
             </div>
