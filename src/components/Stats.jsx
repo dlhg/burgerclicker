929b0a7396f1da87b3decd27cfebd75339390016
dlhg <gragg.drew@gmail.com>
2023-10-25 14:04:07 -0400

    added bug explanation to stats component for anyone testing the game

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,7 +5,10 @@ export default function Stats(props) {
     <>
       <div>Total burgers produced: {props.totalBurgersProduced}</div>
       <div>Burgers from clicking: {props.burgersMadeFromClicking}</div>
-      <div>Burgers from automation: {props.burgersMadeFromAutomation}</div>
+      <div>
+        Burgers from automation: {props.burgersMadeFromAutomation} (this stat
+        currently bugged and showing 2x its true value)
+      </div>
       <div>Burgers per click: {props.burgersPerClick}</div>
     </>
   );
