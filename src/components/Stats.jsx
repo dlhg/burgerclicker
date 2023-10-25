b96a652716f83998610e1fd1851b109c92ec5775
dlhg <gragg.drew@gmail.com>
2023-10-24 21:56:50 -0400

    removed unneeded br in stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Stats(props) {
   return (
     <>
-      <br />
       <div>Total burgers produced: {props.totalBurgersProduced}</div>
       <div>Burgers from clicking: {props.burgersMadeFromClicking}</div>
       <div>Burgers from automation: {props.burgersMadeFromAutomation}</div>
