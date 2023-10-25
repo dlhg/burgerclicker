51e04923c93eaa4e319b1d4fc2ae35b180348d4e
dlhg <gragg.drew@gmail.com>
2023-10-25 12:24:27 -0400

    added some styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,28 @@ import Quips from "./Quips";
 // in cookie clicker, you can also revert back to buildings view by pressing on prev selection again - add this later
 
 export default function Navbar(props) {
-    return (
-        <>
-            <button onClick={() => props.setMainArea("options")}>options</button>
-            <button onClick={() => props.setMainArea("stats")}>stats</button>
-            <Quips
-
-            />
-            <button onClick={() => props.setMainArea("info")}>info</button>
-            <button onClick={() => props.setMainArea("legacy")}>legacy</button>
-            <button onClick={() => props.setMainArea("buildings")}>buildings</button>
-            <button onClick={() => props.setMainArea("testconsole")}>test console</button>
-        </>
-    )
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar--left">
+          <button onClick={() => props.setMainArea("options")}>options</button>
+          <button onClick={() => props.setMainArea("stats")}>stats</button>
+          <button onClick={() => props.setMainArea("info")}>info</button>
+        </div>
+        <div className="navbar--center">
+          <Quips />
+          <p>quips here</p>
+        </div>
+        <div className="navbar--right">
+          <button onClick={() => props.setMainArea("legacy")}>legacy</button>
+          <button onClick={() => props.setMainArea("buildings")}>
+            buildings
+          </button>
+          <button onClick={() => props.setMainArea("testconsole")}>
+            console
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
