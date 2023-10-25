56324a141fd34f8e0ad4ebf02a7f81af90d80451
dlhg <gragg.drew@gmail.com>
2023-10-24 22:29:22 -0400

    added how to play section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,9 +4,21 @@ export default function Info(props) {
   return (
     <>
       <div>
-        Burger Clicker is a project developed by Drew Gragg using React JS. The
-        art was generated using Dalle-3.
+        Burger Clicker is a project developed by Drew Gragg using React JS,
+        Howler, Vite.
       </div>
+      <br />
+      <ul style={{ fontSize: "x-large" }}>
+        <div style={{ fontWeight: "bold", textDecoration: "underline" }}>
+          How to play
+        </div>
+        <li>Click the burger to start making burgers</li>
+        <li>Buy items, buildings, and upgrades with burgers</li>
+        <li>
+          To unlock new store items, your total burgers produced must equal the
+          initial cost of the item
+        </li>
+      </ul>
     </>
   );
 }
