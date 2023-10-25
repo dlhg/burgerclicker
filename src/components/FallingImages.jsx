1d961f0c40d54fb35b7e1e9ff0412fb09d543458
dlhg <gragg.drew@gmail.com>
2023-10-25 01:35:46 -0400

    updated FI component

--- src/components/FallingImages.jsx
+++ src/components/FallingImages.jsx
@@ -1,14 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const FallingImage = (props) => {
   const [fallingImages, setFallingImages] = useState([]);
+  const imageRefs = useRef([]);
 
   useEffect(() => {
     const addFallingImage = () => {
+      const id = Date.now();
       setFallingImages((prevImages) => [
         ...prevImages,
-        { id: Date.now(), top: 0, left: Math.random() * 100 },
+        { id, top: 0, left: Math.random() * 100 },
       ]);
+
+      // Create a ref for the new image
+      const newImageRef = React.createRef();
+      imageRefs.current.push({ id, ref: newImageRef });
+
+      // Remove the image from state after the animation ends
+      const removeImage = () => {
+        setFallingImages((prevImages) =>
+          prevImages.filter((image) => image.id !== id)
+        );
+      };
+
+      // Attach the animationend event listener to the new image using the ref
+      if (newImageRef.current) {
+        newImageRef.current.addEventListener("animationend", removeImage, {
+          once: true,
+        });
+      }
     };
 
     const intervalId = setInterval(addFallingImage, 500);
@@ -21,6 +41,9 @@ const FallingImage = (props) => {
       {fallingImages.map((image) => (
         <img
           key={image.id}
+          ref={
+            imageRefs.current.find((item) => item.id === image.id)?.ref || null
+          }
           className="falling-image"
           src={props.img}
           alt="Falling Image"
