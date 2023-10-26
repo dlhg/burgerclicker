32fea65906b770b40401e4826bb43217f7e5cd0d
dlhg <gragg.drew@gmail.com>
2023-10-26 14:37:47 -0400

    added some todo notes to upgradeitem component for me to work on later

--- src/components/UpgradeItem.jsx
+++ src/components/UpgradeItem.jsx
@@ -1,4 +1,15 @@
-//items earned by getting certain amount of burgs, buildings etc
+/*
+to add:
+- tooltip that shows on hover to show item price, name, and description
+- setter functions for state variables like burgers per click and BPS for individual buildings should be passed in so that upgrades can modify these states
+- add a section in info or somewhere else to show the player all of their current upgrades
+
+
+- once all functionality is set up, move on to styling
+    - upgrade items should have transparent background instead of white
+    - if player's burgers < item cost, then upgrade item should be darkened using css filter
+    - art enhancements (ex. higher tiers of upgrade look increasingly more powerful or at least just different)
+*/
 
 import React, { useState } from "react";
 
