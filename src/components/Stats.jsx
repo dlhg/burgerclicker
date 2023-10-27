1a0a4a8ab374a687ccfa1fae144ba139d102af5b
dlhg <gragg.drew@gmail.com>
2023-10-27 16:51:07 -0400

    added number formatting to stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,105 +1,142 @@
 import React from "react";
+import { formatNumber } from "../utils";
+
+// own X number of X building
+// totalBurgersProduced exceeds X threshold
+// burgersPerSecond exceeds X threshold
+// burgersMadeFromClicking exceeds X threshold
+// burgersMadeFromAutomation exceeds X threshold
+
+/*
+upgrade properties:
+
+- name
+- id
+- icon
+- setter (setter function related to what stat the upgrade improves)
+- argument (argument for setter function)
+- unlockThreshold (how many of X needed to unlock)
+- unlockDependency (ie. )
+
+const allUpgrades = [
+  {
+    totalBurgersProduced:{
+
+    }
+  }
+];
+
+
+*/
 
 export default function Stats(props) {
   return (
     <>
       <section>
         <div>Total burgers produced: {props.totalBurgersProduced}</div>
-        <div>Burgers from clicking: {props.burgersMadeFromClicking}</div>
-        <div>Burgers from automation: {props.burgersMadeFromAutomation}</div>
-        <div>Burgers per click: {props.burgersPerClick}</div>
+        <div>
+          Burgers from clicking: {formatNumber(props.burgersMadeFromClicking)}
+        </div>
+        <div>
+          Burgers from automation:{" "}
+          {formatNumber(props.burgersMadeFromAutomation)}
+        </div>
+        <div>Burgers per click: {formatNumber(props.burgersPerClick)}</div>
       </section>
       <br />
       <section>
         <div>Upgrades unlocked:</div>
         <div className="upgrade--grid">
-          {/*this should be mapping over a list of player upgrades*/}
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
-          <div>test upgrade</div>
+          {/*this should be mapping over a list of owned upgrades*/}
+          {/*if the player doesn't own the upgrade yet, represent it with a question mark pic*/}
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
         </div>
       </section>
       <br />
       <section>
         <div> Achievements unlocked:</div>
         <div className="achievement--grid">
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
-          <div>test achievement</div>
+          {/*this should be mapping over a list of achievements*/}
+          {/*if the player have the achievement yet, represent it with a question mark pic*/}
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
         </div>
       </section>
     </>
