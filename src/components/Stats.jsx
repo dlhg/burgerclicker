159959f2582163df9b3cc0b528e0c07d5c0cd967
dlhg <gragg.drew@gmail.com>
2023-10-27 09:56:20 -0400

    added placement for upgrades and acheievements in stats area

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,10 +3,22 @@ import React from "react";
 export default function Stats(props) {
   return (
     <>
-      <div>Total burgers produced: {props.totalBurgersProduced}</div>
-      <div>Burgers from clicking: {props.burgersMadeFromClicking}</div>
-      <div>Burgers from automation: {props.burgersMadeFromAutomation}</div>
-      <div>Burgers per click: {props.burgersPerClick}</div>
+      <section>
+        <div>Total burgers produced: {props.totalBurgersProduced}</div>
+        <div>Burgers from clicking: {props.burgersMadeFromClicking}</div>
+        <div>Burgers from automation: {props.burgersMadeFromAutomation}</div>
+        <div>Burgers per click: {props.burgersPerClick}</div>
+      </section>
+      <br />
+      <section>
+        <div>Upgrades unlocked:</div>
+        <div>Upgrade grid goes here</div>
+      </section>
+      <br />
+      <section>
+        <div> Achievements unlocked:</div>
+        <div> Achievement grid goes here</div>
+      </section>
     </>
   );
 }
