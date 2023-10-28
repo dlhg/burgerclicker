a45fcd57bdf061a27b0b14912463b63426d9d4b9
dlhg <gragg.drew@gmail.com>
2023-10-28 04:15:57 -0400

    added a SaveLoadGame component with some comments on future functionality

---
+++ src/components/SaveLoadGame.jsx
@@ -0,0 +1,8 @@
+/*
+loadgame takes an object with all important state variables and then runs setters to set to those values
+
+savegame takes all important state variables and saves to an object
+
+possibly add base-64 encoding and decoding
+
+*/
