be5708a7e7f4a6ec13b5747ecfe60e2ba0e4fa70
dlhg <gragg.drew@gmail.com>
2023-10-29 12:57:03 -0400

    updated lucky burger cat flavor text

--- src/components/UpgradeStore.jsx
+++ src/components/UpgradeStore.jsx
@@ -24,9 +24,7 @@ export default function UpgradeStore(props) {
             itemImage={catpic}
             itemName={"lucky burger cat"}
             itemDescription={"+1 Burger Per Click"}
-            itemFlavorText={
-              "A stray cat shows up at your door and crawls inside a burger for warmth. Adopt him as your mascot?"
-            }
+            itemFlavorText={"A stray cat shows up. Adopt him as your mascot?"}
             benefitAmount={(prev) => prev + 1}
             unlockedCondition={props.totalBurgersProducedUnformatted >= 10}
             burgerCount={props.burgerCount}
