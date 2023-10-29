27f764e8b8ab72f9263a981c66c550787c9b7185
dlhg <gragg.drew@gmail.com>
2023-10-28 23:15:06 -0400

    working on reorganizing store layout

---
+++ src/components/ToolTip.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/*
+props:
+- Upgrade name
+- Upgrade cost
+- Upgrade descripton
+
+*/
+
+export default function ToopTip(props) {
+  return (
+    <>
+      <div className="section1--bottomhalf"></div>
+    </>
+  );
+}
