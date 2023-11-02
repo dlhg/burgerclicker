85e436b285a5ece12383ff5ee7a45c5be9fa93fd
dlhg <gragg.drew@gmail.com>
2023-11-02 11:52:00 -0400

    changed assembly time label to assembly times since it's showing multiple vals

--- src/components/Minigame_BurgerAssembly.jsx
+++ src/components/Minigame_BurgerAssembly.jsx
@@ -417,7 +417,7 @@ now that we're capturing burger creation time, we can:
                 <h2>reward: {reward}</h2>
                 burgers served: {correctStreak}
                 <br />
-                assembly time: {burgerCreationTimes}
+                assembly times: {burgerCreationTimes}
                 <br />
                 <button onClick={() => setPlayerBurger(burgerOrder)}>
                   cheat
