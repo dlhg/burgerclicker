3b3ef8a93986a9fef546438e664491a914dd0742
dlhg <gragg.drew@gmail.com>
2023-11-06 22:11:59 -0500

    burger now tracks x/y co-ordinates where it was clicked

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -11,21 +11,32 @@ import { formatNumber } from "../utils";
 
 export default function Burger(props) {
   //const [play] = useSound(clicksounds[getRandomIndex(clicksounds)]);
+  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const interval = setInterval(() => {
       props.setBurgersMadeFromAutomation(
-        (prevCount) => prevCount + (props.burgersPerSecond * props.tempBPSBoostMultiplier / 10)
+        (prevCount) =>
+          prevCount +
+          (props.burgersPerSecond * props.tempBPSBoostMultiplier) / 10
       );
     }, 100);
 
     return () => clearInterval(interval);
   }, [props.burgersPerSecond, props.tempBPSBoostMultiplier]);
 
-  function handleBurgerClick() {
-    props.setBurgerCount((prevCount) => prevCount + (props.burgersPerClick * props.tempBPCBoostMultiplier));
+  function handleBurgerClick(e) {
+    setClickPosition({ x: e.clientX, y: e.clientY });
+    console.log(
+      `burger clicked at X/Y ${e.nativeEvent.clientX},${e.nativeEvent.clientY}`
+    );
+    props.setBurgerCount(
+      (prevCount) =>
+        prevCount + props.burgersPerClick * props.tempBPCBoostMultiplier
+    );
     props.setBurgersMadeFromClicking(
-      (prevCount) => prevCount + (props.burgersPerClick * props.tempBPCBoostMultiplier)
+      (prevCount) =>
+        prevCount + props.burgersPerClick * props.tempBPCBoostMultiplier
     );
     //play();
   }
@@ -34,22 +45,25 @@ export default function Burger(props) {
     <div>
       <section>Burgers : {formatNumber(props.displayedBurgerCount)}</section>
       {/* added placeholder conditional styles to show player if their BPS or BPC is currently boosted, update these to something better later */}
-      <section className={props.tempBPSBoostMultiplier > 1 ? 'rainbow--text' : ''}>
-        Per Second : {formatNumber(props.burgersPerSecond * props.tempBPSBoostMultiplier)}
-        {props.tempBPSBoostMultiplier > 1 ? `  (${props.tempBPSBoostMultiplier}x multiplier)` : ""}
-
-
+      <section
+        className={props.tempBPSBoostMultiplier > 1 ? "rainbow--text" : ""}
+      >
+        Per Second :{" "}
+        {formatNumber(props.burgersPerSecond * props.tempBPSBoostMultiplier)}
+        {props.tempBPSBoostMultiplier > 1
+          ? `  (${props.tempBPSBoostMultiplier}x multiplier)`
+          : ""}
       </section>
 
-      <section className={props.tempBPCBoostMultiplier > 1 ? 'rainbow--text' : ''}>
-        Per Click : {formatNumber(props.burgersPerClick * props.tempBPCBoostMultiplier)}
-        {props.tempBPCBoostMultiplier > 1 ? `  (${props.tempBPCBoostMultiplier}x multiplier)` : ""}
+      <section
+        className={props.tempBPCBoostMultiplier > 1 ? "rainbow--text" : ""}
+      >
+        Per Click :{" "}
+        {formatNumber(props.burgersPerClick * props.tempBPCBoostMultiplier)}
+        {props.tempBPCBoostMultiplier > 1
+          ? `  (${props.tempBPCBoostMultiplier}x multiplier)`
+          : ""}
       </section>
-
-
-
-
-
 
       <br />
       <br />
@@ -61,6 +75,10 @@ export default function Burger(props) {
           alt="Burger"
           className="big--burger"
         />
+        <div
+          className="floating-number"
+          style={{ left: clickPosition.x, top: clickPosition.y }}
+        ></div>
         <br />
         <br />
         <br />
