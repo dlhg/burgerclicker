0c4113b0c98aeaa472c8b3b11393acb3de9e74a0
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2023-11-07 21:29:41 -0500

    added docker file and updated vite server config to prepare for fly.io deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //add the server config below
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000
+  }
 })
